a99054dca73a25a5df2aeffd3ea8993ccd594fcd
sbsteacher <id@gmail.com>
2026-04-01 11:57:08 +0900

    댓글 작성

--- src/main/java/com/green/greengram/application/feedcomment/FeedCommentService.java
+++ src/main/java/com/green/greengram/application/feedcomment/FeedCommentService.java
@@ -4,6 +4,9 @@ import com.green.greengram.application.feedcomment.model.FeedCommentDeleteReq;
 import com.green.greengram.application.feedcomment.model.FeedCommentGetReq;
 import com.green.greengram.application.feedcomment.model.FeedCommentGetRes;
 import com.green.greengram.application.feedcomment.model.FeedCommentPostReq;
+import com.green.greengram.entity.Feed;
+import com.green.greengram.entity.FeedComment;
+import com.green.greengram.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,11 +17,25 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class FeedCommentService {
+    private final FeedCommentRepository feedCommentRepository;
     private final FeedCommentMapper feedCommentMapper;
 
     public long postFeedComment(FeedCommentPostReq req) {
-        feedCommentMapper.save(req);
-        return req.getId();
+        //feedCommentMapper.save(req);
+        //return req.getId();
+        User writerUser = new User();
+        writerUser.setId( req.getSignedUserId() );
+
+        Feed feed = new Feed();
+        feed.setId( req.getFeedId() );
+
+        FeedComment newFeedComment = new FeedComment();
+        newFeedComment.setUser( writerUser );
+        newFeedComment.setFeed( feed );
+        newFeedComment.setComment( req.getComment() );
+
+        feedCommentRepository.save( newFeedComment );
+        return newFeedComment.getId();
     }
 
     public List<FeedCommentGetRes> getFeedCommentList(FeedCommentGetReq req) {
